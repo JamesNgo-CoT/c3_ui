abe826e4ec920194a0c31665da0db0f4634fc806
James Ngo <James.Ngo@toronto.ca>
2018-09-04 10:43:24 -0400

    First Commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+c3_ui
+===========
+Describe your app.
+
+For embedded apps:
+------------------
+This app can be embedded on COT Wordpress pages with:
+
+`[cot_app app="c3_ui"][/cot_app]`
+
+Here is the application.json definition
+
+`{"id": "c3_ui", "title": "My App Name", "description": "Describe your app."}`
---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+// The main javascript file for c3_ui.
+// IMPORTANT:
+// Any resources from this project should be referenced using SRC_PATH preprocessor var
+// Ex: let myImage = '/*@echo SRC_PATH*//img/sample.jpg';
+
+$(function () {
+  if (window['cot_app']) { //the code in this 'if' block should be deleted for embedded apps
+    const app = new cot_app("c3_ui",{
+      hasContentTop: false,
+      hasContentBottom: false,
+      hasContentRight: false,
+      hasContentLeft: false,
+      searchcontext: 'INTRA'
+    });
+
+    app.setBreadcrumb([
+      {"name": "c3_ui", "link": "#"}
+    ]).render();
+  }
+  let container = $('#c3_ui_container');
+});
